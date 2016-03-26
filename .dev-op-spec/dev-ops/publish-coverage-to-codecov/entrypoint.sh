6602271ded13d2a4dfab665008c04e80a39e64aa
Chris Dostert <chris.dostert@live.com>
2016-03-26 11:34:13 -0700

    update dev op spec to publish coverage for non-fakes

--- .dev-op-spec/dev-ops/publish-coverage-to-codecov/entrypoint.sh
+++ .dev-op-spec/dev-ops/publish-coverage-to-codecov/entrypoint.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+# generate aggregate coverage profile
 cat engine.coverprofile > coverage.txt && \
 cat core/core.coverprofile >> coverage.txt && \
 cat core/models/models.coverprofile >> coverage.txt && \
 cat core/adapters/containerengine/dockercompose/dockercompose.coverprofile >> coverage.txt && \
 cat core/adapters/templatesrc/git/git.coverprofile >> coverage.txt && \
 cat core/adapters/filesys/os/os.coverprofile >> coverage.txt && \
-cat rest/rest.coverprofile >> coverage.txt && \
+cat rest/rest.coverprofile >> coverage.txt
+
+# strip fakes from coverage profile
+sed -i '/fake/d' coverage.txt
+
 curl -s https://codecov.io/bash | bash -s
