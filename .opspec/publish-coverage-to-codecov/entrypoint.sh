96c37358fd2a98e0197cb85d9f73ed3f6d943df4
Chris Dostert <chris.dostert@nintex.com>
2016-06-02 21:10:57 -0700

    updated opspec

--- .opspec/publish-coverage-to-codecov/entrypoint.sh
+++ .opspec/publish-coverage-to-codecov/entrypoint.sh
@@ -1,14 +1,14 @@
-#!/bin/sh
+#!/bin/sh -e
 
 # generate aggregate coverage profile
-cat engine.coverprofile > coverage.txt && \
-cat core/core.coverprofile >> coverage.txt && \
-cat core/models/models.coverprofile >> coverage.txt && \
-cat core/adapters/containerengine/dockercompose/dockercompose.coverprofile >> coverage.txt && \
-cat core/adapters/filesys/os/os.coverprofile >> coverage.txt && \
+cat engine.coverprofile > coverage.txt
+cat core/core.coverprofile >> coverage.txt
+cat core/models/models.coverprofile >> coverage.txt
+cat core/adapters/containerengine/dockercompose/dockercompose.coverprofile >> coverage.txt
+cat core/adapters/filesys/os/os.coverprofile >> coverage.txt
 cat tcp/tcp.coverprofile >> coverage.txt
 
 # strip fakes from coverage profile
 sed -i '/fake/d' coverage.txt
 
-bash <(curl -s https://codecov.io/bash)
+curl -s https://codecov.io/bash | bash -s
