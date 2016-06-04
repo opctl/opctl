677930e04fdd9dbbb4f574384f6970b1b12f6392
Chris Dostert <chris.dostert@live.com>
2016-06-04 14:41:32 -0700

    fix cover

--- .opspec/publish-coverage-to-codecov/entrypoint.sh
+++ .opspec/publish-coverage-to-codecov/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh -e
 
 # generate aggregate coverage profile
-cat adapters/host/docker/docker.coverprofile > coverage.txt
-cat models/models.coverprofile > coverage.txt
-cat sdk.coverprofile >> coverage.txt
+cat sdk-golang.coverprofile > coverage.txt
 
 # strip fakes from coverage profile
 sed -i '/fake/d' coverage.txt
