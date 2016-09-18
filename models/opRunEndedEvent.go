4186f8a79ca95b5795088be53b53f62f01e98576
Chris Dostert <chrisdostert@users.noreply.github.com>
2016-09-17 17:20:30 -0700

    Events (#15)
    * add OpRunEncounteredErrorEvent
    
    * add outcome constants

--- models/opRunEndedEvent.go
+++ models/opRunEndedEvent.go
@@ -1,5 +1,11 @@
 package models
 
+const (
+  OpRunOutcomeSucceeded = "SUCCEEDED"
+  OpRunOutcomeFailed = "FAILED"
+  OpRunOutcomeKilled = "KILLED"
+)
+
 type OpRunEndedEvent struct {
   OpRunId       string `json:"opRunId"`
   Outcome       string `json:"outcome"`
