9e6e0e941c693d68665cf7e2702081d83b00fefd
Chris Dostert <chrisdostert@users.noreply.github.com>
2016-11-06 20:52:22 -0800

    refactor) update OpRun attributes to support views (#34)

--- pkg/model/opRun.go
+++ pkg/model/opRun.go
@@ -2,8 +2,8 @@ package models
 
 // Represents an op run. From a data structure perspective, op runs are directed rooted trees (aka out-tree's)
 type OpRun struct {
-  Children []string `json:"children,omitempty"`
+  Children []*OpRun `json:"children,omitempty"`
   Id       string `json:"id"`
-  Parent   string `json:"parent,omitempty"`
+  ParentId string `json:"-"`
   OpRef    string `json:"opRef"`
 }
