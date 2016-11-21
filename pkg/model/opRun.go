2d90af6ec816b4e98be66a61dcd97277d0b99024
Chris Dostert <chrisdostert@users.noreply.github.com>
2016-11-20 23:40:54 -0800

    chore) add RootId to OpRun model (#39)

--- pkg/model/opRun.go
+++ pkg/model/opRun.go
@@ -3,6 +3,7 @@ package model
 // Represents an op run. From a data structure perspective, op runs are directed rooted trees (aka out-tree's)
 type OpRun struct {
   Id       string `json:"id"`
-  ParentId string `json:"-"`
   OpRef    string `json:"opRef"`
+  ParentId string `json:"parentId"`
+  RootId   string `json:"rootId"`
 }
