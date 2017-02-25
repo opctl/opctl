d4637cac488b223e5449b3cb48a40a804d2b9bee
Chris Dostert <chris.dostert@live.com>
2017-02-25 10:48:53 -0800

    feat) add numeric param type validation support

--- vendor/github.com/opspec-io/sdk-golang/pkg/validate/param.go
+++ vendor/github.com/opspec-io/sdk-golang/pkg/validate/param.go
@@ -1,12 +1,7 @@
 package validate
 
 import (
-	"errors"
-	"fmt"
 	"github.com/opspec-io/sdk-golang/pkg/model"
-	"github.com/opspec-io/sdk-golang/util/format"
-	"github.com/xeipuuv/gojsonschema"
-	"strings"
 )
 
 // validates an value against a parameter
@@ -22,69 +17,10 @@ func (this validate) Param(
 	switch {
 	case nil != param.String:
 		errs = this.stringParam(value, param.String)
+	case nil != param.Number:
+		errs = this.numberParam(value, param.Number)
 	case nil != param.Socket:
 		errs = this.socketParam(value, param.Socket)
-	}
-	return
-}
-
-// validates an value against a string parameter
-func (this validate) stringParam(
-	rawValue *model.Data,
-	param *model.StringParam,
-) (errs []error) {
-	errs = []error{}
-
-	// handle no value passed
-	if nil == rawValue {
-		errs = append(errs, errors.New("String required"))
-		return
-	}
-
-	value := rawValue.String
-	if "" == value && "" != param.Default {
-		// apply default if value not set
-		value = param.Default
-	}
-
-	// guard no constraints
-	if nil == param.Constraints {
-		return
-	}
-
-	constraintsJsonBytes, err := format.NewJsonFormat().From(param.Constraints)
-	if err != nil {
-		// panic as errs represents validation errors not execution errors
-		panic(err.Error())
-	}
-
-	result, err := gojsonschema.Validate(
-		gojsonschema.NewStringLoader(string(constraintsJsonBytes)),
-		gojsonschema.NewStringLoader(fmt.Sprintf(`"%v"`, value)),
-	)
-	if err != nil {
-		// panic as errs represents validation errors not execution errors
-		panic(err.Error())
-	}
-
-	for _, errString := range result.Errors() {
-		// enum validation errors include `(root) ` prefix we don't want
-		errs = append(errs, errors.New(strings.TrimPrefix(errString.Description(), "(root) ")))
-	}
-
-	return
-}
-
-// validates an value against a network socket parameter
-func (this validate) socketParam(
-	rawValue *model.Data,
-	param *model.SocketParam,
-) (errs []error) {
-	errs = []error{}
-
-	// handle no value passed
-	if nil == rawValue || "" == rawValue.Socket {
-		errs = append(errs, errors.New("Socket required"))
 	}
 	return
 }
---
+++ vendor/github.com/opspec-io/sdk-golang/pkg/validate/stringParam.go
@@ -0,0 +1,60 @@
+package validate
+
+import (
+	"errors"
+	"fmt"
+	"github.com/opspec-io/sdk-golang/pkg/model"
+	"github.com/opspec-io/sdk-golang/util/format"
+	"github.com/xeipuuv/gojsonschema"
+	"strings"
+)
+
+// validates an value against a string parameter
+func (this validate) stringParam(
+	rawValue *model.Data,
+	param *model.StringParam,
+) (errs []error) {
+	errs = []error{}
+
+	// handle no value passed
+	if nil == rawValue {
+		errs = append(errs, errors.New("String required"))
+		return
+	}
+
+	value := rawValue.String
+	if "" == value && "" != param.Default {
+		// apply default if value not set
+		value = param.Default
+	}
+
+	// guard no constraints
+	if paramConstraints := param.Constraints; nil != paramConstraints {
+
+		// perform validations supported by gojsonschema
+		constraintsJsonBytes, err := format.NewJsonFormat().From(paramConstraints)
+		if err != nil {
+			// handle syntax errors specially
+			errs = append(errs, fmt.Errorf("Error interpreting constraints; the op likely has a syntax error.\n Details: %v", err.Error()))
+			return
+		}
+
+		result, err := gojsonschema.Validate(
+			gojsonschema.NewStringLoader(string(constraintsJsonBytes)),
+			gojsonschema.NewStringLoader(fmt.Sprintf(`"%v"`, value)),
+		)
+		if err != nil {
+			// handle syntax errors specially
+			errs = append(errs, fmt.Errorf("Error interpreting constraints; the op likely has a syntax error.\n Details: %v", err.Error()))
+			return
+		}
+
+		for _, errString := range result.Errors() {
+			// enum validation errors include `(root) ` prefix we don't want
+			errs = append(errs, errors.New(strings.TrimPrefix(errString.Description(), "(root) ")))
+		}
+
+	}
+
+	return
+}
