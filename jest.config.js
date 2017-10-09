e0f4ab369000104ae27633811f59cfcce25d090b
chris dostert <chris.dostert@nintex.com>
2017-10-09 23:02:13 +1100

    chore) add ops

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  verbose: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
+};
