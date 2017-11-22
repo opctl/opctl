1e1720828090285a60a6a6f75514d3e701735383
Chris Dostert <chrisdostert@users.noreply.github.com>
2017-11-21 22:17:56 -0800

    chore) pull in latest spec (#322)

--- model/pkg.go
+++ model/pkg.go
@@ -42,6 +42,7 @@ type PkgHandle interface {
 
 type PkgManifest struct {
 	Description string            `yaml:"description"`
+	Icon        string            `yaml:"icon,omitempty"`
 	Inputs      map[string]*Param `yaml:"inputs,omitempty"`
 	Name        string            `yaml:"name"`
 	Outputs     map[string]*Param `yaml:"outputs,omitempty"`
