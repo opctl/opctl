c0db2b222bccd3c1bac005ba4cffdbdf5af9bdfe
Chris Dostert <chris.dostert@live.com>
2020-05-03 17:27:38 -0700

    Return ref instead of name from opctl ls; closes #634

--- cli/internal/core/lser.go
+++ cli/internal/core/lser.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
+	"strings"
 	"text/tabwriter"
 
 	"github.com/opctl/opctl/cli/internal/cliexiter"
@@ -52,14 +54,14 @@ func (ivkr _lsInvoker) Ls(
 	defer _tabWriter.Flush()
 	_tabWriter.Init(ivkr.writer, 0, 8, 1, '\t', 0)
 
-	fmt.Fprintln(_tabWriter, "NAME\tVERSION\tDESCRIPTION")
+	fmt.Fprintln(_tabWriter, "REF\tDESCRIPTION")
 
 	dirHandle := ivkr.dataResolver.Resolve(
 		dirRef,
 		nil,
 	)
 
-	packages, err := ivkr.opLister.List(
+	opsByPath, err := ivkr.opLister.List(
 		ctx,
 		dirHandle,
 	)
@@ -68,9 +70,26 @@ func (ivkr _lsInvoker) Ls(
 		return // support fake exiter
 	}
 
-	for _, _package := range packages {
+	cwd, err := os.Getwd()
+	if nil != err {
+		ivkr.cliExiter.Exit(cliexiter.ExitReq{Message: err.Error(), Code: 1})
+		return // support fake exiter
+	}
 
-		fmt.Fprintf(_tabWriter, "%v\t%v\t%v", _package.Name, _package.Version, _package.Description)
+	for path, op := range opsByPath {
+		opRef := filepath.Join(dirHandle.Ref(), path)
+		if filepath.IsAbs(opRef) {
+			// make absolute paths relative
+			relOpRef, err := filepath.Rel(cwd, opRef)
+			if nil != err {
+				ivkr.cliExiter.Exit(cliexiter.ExitReq{Message: err.Error(), Code: 1})
+				return // support fake exiter
+			}
+
+			opRef = strings.TrimPrefix(relOpRef, ".opspec/")
+		}
+
+		fmt.Fprintf(_tabWriter, "%v\t%v", opRef, op.Description)
 		fmt.Fprintln(_tabWriter)
 
 	}
