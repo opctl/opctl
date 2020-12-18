5f3b24fb371d842cae88052aad03bee56275ebdc
Cameron Little <cameron@remitly.com>
2020-12-18 12:54:58 +0100

    improve test coverate of lser

--- cli/internal/core/lser_test.go
+++ cli/internal/core/lser_test.go
@@ -1,13 +1,16 @@
 package core
 
 import (
+	"bytes"
 	"context"
 	"errors"
 	"os"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	clioutput "github.com/opctl/opctl/cli/internal/clioutput/fakes"
 	dataresolver "github.com/opctl/opctl/cli/internal/dataresolver/fakes"
+	"github.com/opctl/opctl/sdks/go/model"
 	. "github.com/opctl/opctl/sdks/go/model/fakes"
 )
 
@@ -23,10 +26,7 @@ var _ = Context("Lser", func() {
 			fakeDataResolver := new(dataresolver.FakeDataResolver)
 			fakeDataResolver.ResolveReturns(fakeOpHandle, nil)
 
-			objectUnderTest := _lsInvoker{
-				dataResolver: fakeDataResolver,
-				writer:       os.Stdout,
-			}
+			objectUnderTest := newLser(new(clioutput.FakeCliOutput), fakeDataResolver)
 
 			/* act */
 			err := objectUnderTest.Ls(
@@ -41,12 +41,29 @@ var _ = Context("Lser", func() {
 			Expect(actualDirRef).To(Equal(providedDirRef))
 			Expect(actualPullCreds).To(BeNil())
 		})
-		Context("opLister.List errors", func() {
-			It("should return expected error", func() {
+		It("should return dataResolver.Resolve errors", func() {
+			/* arrange */
+			expectedError := errors.New("expected")
+			fakeDataResolver := new(dataresolver.FakeDataResolver)
+			fakeDataResolver.ResolveReturns(nil, expectedError)
+
+			objectUnderTest := newLser(new(clioutput.FakeCliOutput), fakeDataResolver)
+
+			/* act */
+			err := objectUnderTest.Ls(context.Background(), "dummy")
+
+			/* assert */
+			Expect(err).To(MatchError(err))
+		})
+		Context("opLister.List", func() {
+			It("works", func() {
 				/* arrange */
 				fakeOpHandle := new(FakeDataHandle)
-				fakeOpHandle.ListDescendantsReturns(nil, errors.New(""))
-
+				fakeOpHandle.ListDescendantsReturns([]*model.DirEntry{{
+					Path: "/path/op.yml",
+				}}, nil)
+				rs := bytes.NewReader([]byte(`{"name": "validater_valid"}`))
+				fakeOpHandle.GetContentReturns(mockReadSeekCloser{rs}, nil)
 				fakeDataResolver := new(dataresolver.FakeDataResolver)
 				fakeDataResolver.ResolveReturns(fakeOpHandle, nil)
 
@@ -56,13 +73,29 @@ var _ = Context("Lser", func() {
 				}
 
 				/* act */
-				err := objectUnderTest.Ls(
-					context.Background(),
-					"dummyDirRef",
-				)
+				err := objectUnderTest.Ls(context.Background(), "dummyDirRef")
 
 				/* assert */
-				Expect(err).To(MatchError(""))
+				Expect(err).To(BeNil())
+			})
+			It("should return errors", func() {
+				/* arrange */
+				expectedError := errors.New("expected")
+				fakeOpHandle := new(FakeDataHandle)
+				fakeOpHandle.ListDescendantsReturns(nil, expectedError)
+				fakeDataResolver := new(dataresolver.FakeDataResolver)
+				fakeDataResolver.ResolveReturns(fakeOpHandle, nil)
+
+				objectUnderTest := _lsInvoker{
+					dataResolver: fakeDataResolver,
+					writer:       os.Stdout,
+				}
+
+				/* act */
+				err := objectUnderTest.Ls(context.Background(), "dummyDirRef")
+
+				/* assert */
+				Expect(err).To(MatchError(expectedError))
 			})
 		})
 	})
