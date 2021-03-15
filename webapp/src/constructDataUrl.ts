cc4d15f6b4c4d14f06285acef40e1b9bcfb06506
= <=>
2021-03-14 22:58:08 -0700

    Update UI to use universal data API

--- webapp/src/constructDataUrl.ts
+++ webapp/src/constructDataUrl.ts
@@ -1,7 +1,5 @@
-import path from 'path'
-
 export default function constructDataUrl(
     dataRef: string
 ): string {
-    return `http://localhost:42224/api/pkgs/${encodeURIComponent(path.dirname(dataRef))}/contents/${encodeURIComponent(path.basename(dataRef))}`
+    return `http://localhost:42224/api/data/${encodeURIComponent(dataRef)}`
 }
