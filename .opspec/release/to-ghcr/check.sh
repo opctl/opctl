25b86dda1f5e0752e289db42a3483a6b35c18969
Chris Spalding <chrissp@remitly.com>
2024-06-17 14:38:54 -0700

    boolean instead of number

--- .opspec/release/to-ghcr/check.sh
+++ .opspec/release/to-ghcr/check.sh
@@ -7,8 +7,8 @@ apk add --update docker
 # only continue with release if tag doesn't exist
 if docker pull ghcr.io/opctl/opctl:${version}-dind; then
   echo "Opctl Image for version '${version}' already exists"
-  echo 1 | tr -d '\n' > /alreadyPublished
+  echo true | tr -d '\n' > /alreadyPublished
 else
   echo "Image does not exist, proceeding with release..."
-  echo 0 | tr -d '\n' > /alreadyPublished
+  echo false | tr -d '\n' > /alreadyPublished
 fi
