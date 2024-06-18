4962fa680cdbc81a42ca42fa6d6f29d64cd9ec50
Chris Spalding <chrissp@remitly.com>
2024-06-18 16:11:29 -0700

    yeet4

--- .opspec/release/test.sh
+++ .opspec/release/test.sh
@@ -1,3 +1,3 @@
 apk add --update github-cli
 
-gh api --verbose -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /orgs/opctl/packages\?package_type\=container
+gh api --verbose -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /orgs/opctl/packages?package_type=container
