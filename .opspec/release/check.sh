13b94ce1199e74e3c5231c73b073537c66ffa98a
Chris Spalding <chrissp@remitly.com>
2024-06-17 17:04:03 -0700

    Update .opspec/release/check.sh
    Co-authored-by: Chris Dostert <chrisdostert@users.noreply.github.com>
    Signed-off-by: Chris Spalding <chrissp@remitly.com>

--- .opspec/release/check.sh
+++ .opspec/release/check.sh
@@ -7,7 +7,7 @@ apk add --update docker
 # only continue with release if tag doesn't exist
 if docker pull ghcr.io/opctl/opctl:${version}-dind; then
   echo "Opctl Image for version '${version}' already exists"
-  echo true | tr -d '\n' > /alreadyPublished
+  echo true > /alreadyPublished
 else
   echo "Image does not exist, proceeding with release..."
   echo false | tr -d '\n' > /alreadyPublished
