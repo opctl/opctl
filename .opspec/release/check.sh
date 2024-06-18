185f1ed13be5172756266813ec064da5767558a6
Chris Spalding <chrissp@remitly.com>
2024-06-18 15:19:41 -0700

    fail faster

--- .opspec/release/check.sh
+++ .opspec/release/check.sh
@@ -1,14 +1,15 @@
-#!/bin/sh
-
-set -e
-
 apk add --update docker
 
+error=$(docker pull ghcr.io/opctl/opctl:${version}-dind 2>&1 1>/dev/null)
+
 # only continue with release if tag doesn't exist
-if docker pull ghcr.io/opctl/opctl:${version}-dind; then
+if [[ -z $error ]]; then
   echo "Opctl Image for version '${version}' already exists"
   echo -n true > /alreadyPublished
+elif [[ $error =~ "denied" ]]; then
+  echo "unable to read from ghcr with given credentials"
+  exit 1
 else
-  echo "Image does not exist, proceeding with release..."
+  echo "Image does not exist for version '${version}', proceeding with release..."
   echo -n false > /alreadyPublished
 fi
