ea5e5b0f13c29e8c3805b09e988f0f22b378f7a2
Chris Spalding <chrissp@remitly.com>
2024-06-20 08:22:35 -0700

    yeet7

--- .opspec/release/test.sh
+++ .opspec/release/test.sh
@@ -1,3 +1,7 @@
-apk add --update github-cli
+apk add --update curl jq
 
-gh api --verbose -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /orgs/opctl/packages?package_type=container
+curl --verbose \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  -H "Authorization: Bearer ${GH_TOKEN}"\
+  https://api.github.com/orgs/opctl/packages?package_type=container | jq
