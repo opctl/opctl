ee5f3e2b98d8cf7860556a1ec8a040ad7d399656
Chris Spalding <chrissp@remitly.com>
2024-06-20 10:40:17 -0700

    Don't run duplicate builds, fix some warnings from older versions of reusable actions, fix check for existing image
    undo some random changes
    
    fix: whitespace

--- .opspec/release/check.sh
+++ .opspec/release/check.sh
@@ -1,6 +1,6 @@
 apk add --update docker
 
-error=$(docker inspect ghcr.io/opctl/opctl:${version}-dind 2>&1 1>/dev/null)
+error=$(docker pull ghcr.io/opctl/opctl:${version}-dind 2>&1 1>/dev/null)
 
 # only continue with release if tag doesn't exist
 if [[ -z $error ]]; then
@@ -10,6 +10,7 @@ elif [[ $error =~ "denied" ]]; then
   echo "unable to read from ghcr with given credentials"
   exit 1
 else
+  echo "error was ${error}"
   echo "Image does not exist for version '${version}', proceeding with release..."
   echo -n false > /alreadyPublished
 fi
--- .opspec/release/test.sh
+++
@@ -1,9 +0,0 @@
-apk add --update curl jq github-cli
-
-gh api --verbose -H "Accept: application/vnd.github+json" -H "X-GitHub-Api-Version: 2022-11-28" /orgs/opctl/packages?package_type=container
-
-curl --verbose \
-  --user "${GITHUB_ACTOR}:${GITHUB_TOKEN}" \
-  -H "Accept: application/vnd.github+json" \
-  -H "X-GitHub-Api-Version: 2022-11-28" \
-  -X GET https://api.github.com/orgs/opctl/packages?package_type=container | jq
