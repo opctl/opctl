5e30a71a37e3a381e537aa4be373110ce72ff2f3
Caleb McCombs <calebm@remitly.com>
2025-02-05 13:40:45 -0600

    Allow Failure In GPU Support Check To Just Mean GPU Passthrough Is Not Supported

--- sdks/go/node/containerruntime/docker/runContainer.go
+++ sdks/go/node/containerruntime/docker/runContainer.go
@@ -141,7 +141,8 @@ func (cr _runContainer) RunContainer(
 
 	isGpuSupported, err := isGpuSupported(ctx, cr.dockerClient, req.Image.PullCreds)
 	if nil != err {
-		return nil, err
+		// Failure to determine GPU support just really means no, GPU is not supported.
+		isGpuSupported = false
 	}
 
 	// create container
