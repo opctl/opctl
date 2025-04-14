a4e703b22b0692532b04c2521f5c595bc03a6aca
Chris Dostert <chris.dostert@icloud.com>
2025-04-13 22:38:48 -0700

    Add oauth2 support for github.com

--- cli/internal/dataresolver/dataResolver.go
+++ cli/internal/dataresolver/dataResolver.go
@@ -13,6 +13,9 @@ import (
 	dataNode "github.com/opctl/opctl/sdks/go/data/node"
 	"github.com/opctl/opctl/sdks/go/model"
 	"github.com/opctl/opctl/sdks/go/node"
+	"github.com/skratchdot/open-golang/open"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/endpoints"
 )
 
 // DataResolver resolves packages
@@ -57,10 +60,6 @@ func (dtr _dataResolver) Resolve(
 	domain := strings.Split(dataRef, "/")[0]
 
 	passwordDescription := fmt.Sprintf("Password for %s.", domain)
-	if domain == "github.com" {
-		// customize github.com password description...
-		passwordDescription = "Personal access token for github.com with 'Repo' permissions."
-	}
 
 	credsPromptInputs := map[string]*model.ParamSpec{
 		usernameInputName: {
@@ -100,6 +99,19 @@ func (dtr _dataResolver) Resolve(
 		}
 
 		if model.IsAuthError(err) && !reattemptedAuth {
+			if domain == "github.com" {
+				t, err := authenticateWithGitHub(ctx)
+				if err != nil {
+					return nil, err
+				}
+
+				pullCreds = &model.Creds{
+					Username: "n/a",
+					Password: t.AccessToken,
+				}
+				reattemptedAuth = true
+				continue
+			}
 			// auth errors can be fixed by supplying correct creds so don't give up; prompt
 			cliPromptInputSrc := dtr.cliParamSatisfier.NewCliPromptInputSrc(credsPromptInputs)
 
@@ -128,3 +140,35 @@ const (
 	usernameInputName = "username"
 	passwordInputName = "password"
 )
+
+func authenticateWithGitHub(
+	ctx context.Context,
+) (*oauth2.Token, error) {
+	config := &oauth2.Config{
+		// safe to embed per https://github.com/cli/cli/blob/7d558edd12d6b4101a0b3d3df3c689385a1c830a/internal/authflow/flow.go#L21-L25
+		ClientID:     "Ov23liVQSZYxmPWGLNGe",
+		ClientSecret: "48d696d29d06eab46ef562535d64775f852e343b",
+		Endpoint:     endpoints.GitHub,
+		Scopes:       []string{"repo"},
+	}
+
+	// Initiate device flow
+	deviceCode, err := config.DeviceAuth(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get device code: %w", err)
+	}
+
+	// Display instructions to the user
+	fmt.Printf("Please visit %s and enter the code: %s\n", deviceCode.VerificationURI, deviceCode.UserCode)
+	fmt.Println("Waiting for authorization...")
+
+	err = open.Run(
+		deviceCode.VerificationURI,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	// Poll for the access token
+	return config.DeviceAccessToken(ctx, deviceCode)
+}
--- go.mod
+++ go.mod
@@ -36,6 +36,7 @@ require (
 	github.com/spf13/pflag v1.0.5
 	github.com/xeipuuv/gojsonschema v1.2.0
 	golang.org/x/net v0.31.0
+	golang.org/x/oauth2 v0.19.0
 	golang.org/x/sync v0.9.0
 	golang.org/x/term v0.26.0
 	golang.zx2c4.com/wireguard v0.0.0-20231211153847-12269c276173
@@ -182,7 +183,6 @@ require (
 	golang.org/x/crypto v0.29.0 // indirect
 	golang.org/x/exp v0.0.0-20240719175910-8a7402abbf56 // indirect
 	golang.org/x/mod v0.22.0 // indirect
-	golang.org/x/oauth2 v0.19.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
 	golang.org/x/text v0.20.0 // indirect
 	golang.org/x/time v0.5.0 // indirect
